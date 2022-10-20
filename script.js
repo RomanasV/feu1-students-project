c66a59b83dcd62388304d08326d619a897dc5fe8
RomanasV <romanasvenckus@gmail.com>
2022-10-20 14:27:37 +0300

    script.js file cleaned

--- script.js
+++ script.js
@@ -1,64 +1,4 @@
-// 1. initialData masyvą įdėti į localStorage.
-
-// let initialData = [
-//   {
-//     name: 'John',
-//     surname: 'Doe',
-//     age: 31,
-//     phone: 4565464645,
-//     email: 'name@surname.com',
-//     itKnowledge: 8,
-//     group: 'feu 5',
-//     interests: ['JavaScript', 'PHP'],
-//   },
-//   {
-//     name: 'Doe',
-//     surname: 'John',
-//     age: 18,
-//     phone: 87964631321,
-//     email: 'name2@surname.com',
-//     itKnowledge: 4,
-//     group: 'feu 3',
-//     interests: [],
-//   },
-//   {
-//     name: 'Tom',
-//     surname: 'John',
-//     age: 43,
-//     phone: 87964631321,
-//     email: 'name2@surname.com',
-//     itKnowledge: 10,
-//     group: 'feu 1',
-//     interests: ['C++', 'PHP'],
-//   },
-//   {
-//     name: 'Steve',
-//     surname: 'John',
-//     age: 21,
-//     phone: 87964631321,
-//     email: 'name2@surname.com',
-//     itKnowledge: 1,
-//     group: 'feu 4',
-//     interests: ['PHP', 'Node.js', 'JavaScript'],
-//   },
-//   {
-//     name: 'Patrick',
-//     surname: 'John',
-//     age: 23,
-//     phone: 87964631321,
-//     email: 'name2@surname.com',
-//     itKnowledge: 7,
-//     group: 'feu 2',
-//     interests: ['PHP'],
-//   },
-// ];
-
-// localStorage.setItem('students-data', JSON.stringify(initialData));
-
-// 2. Užkrovus puslapį iš localStorage išimti šį masyvą ir jį pridėti prie localStorageStudentData kintamojo.
-let localStorageStudentsData = JSON.parse(localStorage.getItem('students-data')); // studentu masyvas paimtas is localStorage
-
-// let studentForm = document.getElementById('student-form');
+let localStorageStudentsData = JSON.parse(localStorage.getItem('students-data'));
 let studentForm = document.querySelector('#student-form');
 let studentsList = document.querySelector('#students-list');
 
@@ -74,7 +14,6 @@ function renderSingleStudent(data) {
   let email = data.email;
   let itKnowledge = data.itKnowledge;
   let group = data.group;
-  // let interests = document.querySelectorAll('[name="interest"]:checked');
   let interests = data.interests;
 
   let inputErrorMessages = studentForm.querySelectorAll('.input-error-message');
@@ -215,20 +154,6 @@ function renderSingleStudent(data) {
   localStorage.removeItem('interest');
 }
 
-// let form = document.querySelector('form');
-
-// form.addEventListener('submit', (event) => {
-//   event.preventDefault();
-
-//   let name = document.querySelector('#name').value;
-//   let age = document.querySelector('input[name=age]');
-
-//   console.dir(name);
-// });
-
-
-
-
 function changeRangeOutput() {
   let itKnowledgeInput = document.querySelector('#student-it-knowledge');
   let itKnowledgeOutput = document.querySelector('#it-knowledge-output');
@@ -242,19 +167,6 @@ changeRangeOutput();
 
 studentForm.addEventListener('submit', (event) => {
   event.preventDefault();
-  
-  // let name = document.querySelector('#student-name').value;
-  // let surname = document.getElementById('student-surname').value;
-  // let age = document.querySelector('[name="age"]').value;
-  // let phone = document.getElementsByName('phone')[0].value;
-  // let email = studentForm.querySelector('#student-email').value;
-
-  // let name = event.target.elements.name.value;
-  // let surname = event.target.elements.surname.value;
-  // let age = event.target.elements.age.value;
-  // let phone = event.target.elements.phone.value;
-  // // let email = event.target.elements['student-email'].value;
-  // let email = event.target.elements.email.value;
 
   let elements = event.target.elements;
 
@@ -262,10 +174,8 @@ studentForm.addEventListener('submit', (event) => {
   let surname = elements.surname.value;
   let age = elements.age.value;
   let phone = elements.phone.value;
-  // let email = elements['student-email'].value;
   let email = elements.email.value;
   let itKnowledge = elements['it-knowledge'].value;
-  // let group = document.querySelector('[name="group"]:checked');
   let group = elements.group.value;
   let interests = document.querySelectorAll('[name="interest"]:checked');
 
@@ -286,60 +196,29 @@ studentForm.addEventListener('submit', (event) => {
         formIsValid = false;
         let errorText = 'Name is too short. At least 3 symbols is required.'
         checkInputData(input, errorText);
-        // let inputErrorMessage = document.createElement('span');
-        // inputErrorMessage.classList.add('input-error-message', 'color-red');
-        // input.classList.add('input-error');
-        // input.after(inputErrorMessage);
-        // inputErrorMessage.textContent = 'Name is too short. At least 3 symbols is required.';
       }
     } else if (input.name === 'surname') {
       if (input.value.length < 3) {
         formIsValid = false;
         checkInputData(input, 'Surname is too short. At least 3 symbols is required.');
-        // let inputErrorMessage = document.createElement('span');
-        // inputErrorMessage.classList.add('input-error-message', 'color-red');
-        // input.classList.add('input-error');
-        // input.after(inputErrorMessage);
-        // inputErrorMessage.textContent = 'Surname is too short. At least 3 symbols is required.';
       }
     } else if (input.name === 'phone') {
       if (input.value.length < 9 || input.value.length > 12) {
         formIsValid = false;
         checkInputData(input, 'Phone number is invalid.');
-        // let inputErrorMessage = document.createElement('span');
-        // inputErrorMessage.classList.add('input-error-message', 'color-red');
-        // input.classList.add('input-error');
-        // input.after(inputErrorMessage);
-        // inputErrorMessage.textContent = 'Phone number is invalid.';
       }
     } else if (input.name === 'age') {
       if (input.value < 0) {
         formIsValid = false;
         checkInputData(input, 'Age cannot be a negative number.');
-        // let inputErrorMessage = document.createElement('span');
-        // inputErrorMessage.classList.add('input-error-message', 'color-red');
-        // input.classList.add('input-error');
-        // input.after(inputErrorMessage);
-        // inputErrorMessage.textContent = 'Age cannot be a negative number.';
       } else if (input.value > 120) {
         formIsValid = false;
         checkInputData(input, 'Age cannot be more then 120 years.');
-        // let inputErrorMessage = document.createElement('span');
-        // inputErrorMessage.classList.add('input-error-message', 'color-red');
-        // input.classList.add('input-error');
-        // input.after(inputErrorMessage);
-        // inputErrorMessage.textContent = 'Age cannot be more then 120 years.';
-
       }
     } else if (input.name === 'email') {
       if (input.value.length < 9 || !input.value.includes('@') || !input.value.includes('.')) {
         formIsValid = false;
         checkInputData(input, 'Email is incorrect.');
-        // let inputErrorMessage = document.createElement('span');
-        // inputErrorMessage.classList.add('input-error-message', 'color-red');
-        // input.classList.add('input-error');
-        // input.after(inputErrorMessage);
-        // inputErrorMessage.textContent = 'Email is incorrect.';
       }
     }
   });
@@ -394,23 +273,6 @@ studentForm.addEventListener('submit', (event) => {
   privateInfoButton.textContent = 'Show personal info';
   privateInfoButton.classList.add('private-info-button', 'show');
 
-  // privateInfoButton.addEventListener('click', () => {
-  //   let privateEmail = emailElement.querySelector('.hidden-area');
-  //   let privatePhone = phoneElement.querySelector('.hidden-area');
-
-  //   if (privateInfoButton.classList.contains('show')) {
-  //     privateEmail.textContent = email;
-  //     privatePhone.textContent = phone;
-  //     privateInfoButton.textContent = 'Hide personal info';
-  //   } else {
-  //     privateEmail.textContent = '****';
-  //     privatePhone.textContent = '****';
-  //     privateInfoButton.textContent = 'Show personal info';
-  //   }
-
-  //   privateInfoButton.classList.toggle('show');
-  // });
-
   let dataHidden = true;
 
   privateInfoButton.addEventListener('click', () => {
@@ -445,42 +307,10 @@ studentForm.addEventListener('submit', (event) => {
   let createdStudentText = `Student created (${name} ${surname})`;
   renderAlertMessage(createdStudentText);
 
-  // 3. Kontaktų forma turi išsivalyti.
   event.target.reset();
 
-  // 1. Iš localStorage paimti students-data masyvą ir jį išsiparsinti.
   let localStorageStudentsData = JSON.parse(localStorage.getItem('students-data'));
-  // 2. Sukurti naujo studento objektą pagal pateiktą pavyzdį.
-
-  // let interestValues = [...interests].map(interest => {
-  //   return interest.value;
-  // });
-
   let interestValues = [...interests].map(interest => interest.value);
-
-  // PAVYZDYS:
-
-  // {
-  //     name: 'John',
-  //     surname: 'Doe',
-  //     age: 31,
-  //     phone: 4565464645,
-  //     email: 'name@surname.com',
-  //     itKnowledge: 8,
-  //     group: 'feu 5',
-  //     interests: ['JavaScript', 'PHP'],
-//   }
-
-  // let createdStudent = {
-  //   name: name,
-  //   surname: surname,
-  //   age: age,
-  //   email: email,
-  //   phone: phone,
-  //   itKnowledge: itKnowledge,
-  //   group: group,
-  //   interests: interestValues,
-  // }
 
   let createdStudent = {
     name,
@@ -493,14 +323,8 @@ studentForm.addEventListener('submit', (event) => {
     interests: interestValues,
   }
 
-  // 3. Naujo studento objektą pridėti (push) į studentų masyva (kuris gautas iš pirmos užduoties).
   localStorageStudentsData.push(createdStudent);
-
-  // 4. Atnaujintą studentų masyvą pridėti į localStorage (nepamiršti stringify metodo).
   localStorage.setItem('students-data', JSON.stringify(localStorageStudentsData));
-
-
-
 
   localStorage.removeItem('name');
   localStorage.removeItem('surname');
@@ -543,142 +367,6 @@ let itKnowledgeInput = document.getElementById('student-it-knowledge');
 let groupInputs = document.querySelectorAll('[name="group"]');
 let interestInputs = document.querySelectorAll('[name="interest"]');
 
-// nameInput.value = localStorage.getItem('student-name');
-// surnameInput.value = localStorage.getItem('student-surname');
-// ageInput.value = localStorage.getItem('student-age');
-// phoneInput.value = localStorage.getItem('student-phone');
-// emailInput.value = localStorage.getItem('student-email');
-// itKnowledgeInput.value = localStorage.getItem('student-it-knowledge');
-
-// nameInput.addEventListener('input', () => {
-//   localStorage.setItem('student-name', nameInput.value);
-// });
-
-// surnameInput.addEventListener('input', () => {
-//   localStorage.setItem('student-surname', surnameInput.value);
-// })
-
-// ageInput.addEventListener('input', () => {
-//   // localStorage.setItem(ageInput.id, ageInput.value);
-
-//   let storageKey = ageInput.id;
-//   let storageValue = ageInput.value;
-//   localStorage.setItem(storageKey, storageValue);
-// });
-
-// phoneInput.addEventListener('input', () => {
-//   let storageKey = phoneInput.id;
-//   let storageValue = phoneInput.value;
-//   localStorage.setItem(storageKey, storageValue);
-// });
-
-// emailInput.addEventListener('input', (event) => {
-//   let storageKey = event.target.id;
-//   let storageValue = event.target.value;
-//   localStorage.setItem(storageKey, storageValue);
-// });
-
-// itKnowledgeInput.addEventListener('input', (event) => {
-//   let storageKey = event.target.id;
-//   let storageValue = event.target.value;
-//   localStorage.setItem(storageKey, storageValue);
-// });
-
-// LOCAL STORAGE 1
-// function populateSimpleInput(input) {
-//   let oldStorageValue = localStorage.getItem(input.id);
-//   if (oldStorageValue !== null) {
-//     input.value = oldStorageValue;
-//   }
-
-//   input.addEventListener('input', (event) => {
-//     let storageKey = event.target.id;
-//     let storageValue = event.target.value;
-//     localStorage.setItem(storageKey, storageValue);
-//   });
-// }
-
-// function populateRadioInput(elements) {
-//   let oldStorageValue = localStorage.getItem('group');
-//   document.getElementById(oldStorageValue).checked = true;
-
-//   elements.forEach((element) => {
-//     element.addEventListener('input', () => {
-//       localStorage.setItem(element.name, element.id);
-//     });
-//   });
-// }
-
-// function populateCheckboxInputs(elements) {
-//   let localStorageInterests = JSON.parse(localStorage.getItem('interests'));
-
-//   localStorageInterests.map(id => {
-//     document.getElementById(id).checked = true;
-//   });
-  
-//   elements.forEach((element) => {
-//     element.addEventListener('input', () => {
-//       let studentInterests = document.querySelectorAll('[name="interest"]:checked');
-
-//       let studentInterestsArr = [];
-
-//       studentInterests.forEach(interest => {
-//         studentInterestsArr.push(interest.id);
-//       });
-
-//       localStorage.setItem('interests', JSON.stringify(studentInterestsArr));
-//     })
-//   })
-// }
-
-// populateSimpleInput(nameInput);
-// populateSimpleInput(surnameInput);
-// populateSimpleInput(ageInput);
-// populateSimpleInput(phoneInput);
-// populateSimpleInput(emailInput);
-// populateSimpleInput(itKnowledgeInput);
-// populateRadioInput(groupInputs);
-// populateCheckboxInputs(interestInputs);
-
-
-// LOCAL STORAGE 2
-// function formDataInLocalStorage(form) {
-//   let localName = localStorage.getItem('name');
-//   let localSurname = localStorage.getItem('surname');
-//   let localAge = localStorage.getItem('age');
-//   let localPhone = localStorage.getItem('phone');
-//   let localEmail = localStorage.getItem('email');
-//   let localItKnowledge = localStorage.getItem('it-knowledge');
-//   let localGroup = localStorage.getItem('group');
-
-//   let nameInput = form.elements.name;
-//   let surnameInput = form.elements.surname;
-//   let ageInput = form.elements.age;
-//   let phoneInput = form.elements.phone;
-//   let emailInput = form.elements.email;
-//   let itKnowledgeInput = form.elements['it-knowledge'];
-//   let groupInput = form.elements.group;
-
-//   nameInput.value = localName;
-//   surnameInput.value = localSurname;
-//   ageInput.value = localAge;
-//   phoneInput.value = localPhone;
-//   emailInput.value = localEmail;
-//   itKnowledgeInput.value = localItKnowledge;
-//   groupInput.value = localGroup;
-
-//   form.addEventListener('input', (event) => {
-//     let activeInput = event.target;
-//     let inputName = activeInput.name;
-//     let inputValue = activeInput.value;
-
-//     localStorage.setItem(inputName, inputValue);
-//   })
-// }
-
-// formDataInLocalStorage(studentForm);
-
-// LOCAL STORAGE 2 (naudojant objektą)
 function formDataInLocalStorage(form) {
   let localName = localStorage.getItem('name');
   let localSurname = localStorage.getItem('surname');
@@ -732,29 +420,19 @@ function formDataInLocalStorage(form) {
 
 formDataInLocalStorage(studentForm);
 
-
 function filterStudents() {
-  // 1. Selektinti paieškos forma javascript'e ir priskirti ją kintamąjam.
   let searchForm = document.querySelector('#search-form');
-  // 2. Šiam kintamąjam pridėti event listener'į - jo tipas submit.
   searchForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    // 3. Submit metu, išsaugoti duomenis, kurie įvesti paieškos formoje (text input'e).
     let searchInput = event.target.elements.search.value.toLowerCase();
-    // + 3.1. Submit metu, išsaugoti select elemento value.
     let searchVariations = event.target.elements.variations.value;
-    // 4. Selektinti visus studentų elementus, juos pridedam į kintamąjį.
     let allStudents = document.querySelectorAll('.student-item');
-    // 5. Leisti ciklą per studentų masyvą ir kiekvieno ciklo metu:
     allStudents.forEach(student => {
-      // 5.1. Paselektinti studento vardą.
       let studentName = student.querySelector('.student-name').textContent.toLowerCase();
-      // 5.2. Paselektinti studento pavardę.
       let studentSurname = student.querySelector('.student-surname').textContent.toLowerCase();
       let studentGroup = student.querySelector('.student-group').textContent.toLowerCase();
       let studentAge = student.querySelector('.student-age').textContent;
       let studentItKnowledge = student.querySelector('.student-it-knowledge').textContent;
-      console.log(studentItKnowledge);
 
       let displayStr = '';
 
@@ -773,35 +451,8 @@ function filterStudents() {
       }
 
       student.style.display = displayStr;
-
-      // 5.3. Patikrinti ar varde arba pavardėje yra ieškoma frazė.
-      // if (studentName.includes(searchInput) || studentSurname.includes(searchInput)) {
-      //   // 5.3.2. Jeigu yra, tai reikia parodyti studento elementą (display: block).
-      //   student.style.display = 'block';
-      // } else {
-      //   // 5.3.1. Jeigu nėra, tai reikia paslėpti studento elementą (display: none).
-      //   student.style.display = 'none';
-      // }
     });
   })
 }
 
 filterStudents();
-
-
-
-// 1. Selektinti paieškos forma javascript'e ir priskirti ją kintamąjam.
-// 2. Šiam kintamąjam pridėti event listener'į - jo tipas submit.
-// 3. Submit metu, išsaugoti duomenis, kurie įvesti paieškos formoje (text input'e).
-// + 3.1. Submit metu, išsaugoti select elemento value.
-// 4. Selektinti visus studentų elementus, juos pridedam į kintamąjį.
-// 5. Leisti ciklą per studentų masyvą ir kiekvieno ciklo metu:
-// 5.1. Paselektinti studento vardą.
-// 5.2. Paselektinti studento pavardę.
-// + 5.3. Paselektinti studento amžių.
-// + 5.4. Paselektinti studento grupę.
-// + 5.5. Paselektinti studento it žinias.
-// - 5.3. Patikrinti ar varde arba pavardėje yra ieškoma frazė.
-// + 5.3. Priklausomai nuo to, kuri vertė yra pasirinkta select elemente, patikrinti ar nurodytame elemente yra paieškos frazė.
-// 5.3.1. Jeigu nėra, tai reikia paslėpti studento elementą (display: none).
-// 5.3.2. Jeigu yra, tai reikia parodyti studento elementą (display: block).
